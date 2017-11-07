9cbb8841ba322867a5b514b0182bc82d00a0deb4
annabelckim <ankim@packer.edu>
2017-11-07 16:58:53 -0500

    i submited my ice cream

--- Ice cream cone /sketch.js
+++ Ice cream cone /sketch.js
@@ -1,7 +1,15 @@
 function setup() {
   createCanvas (600,600);
+  background (179, 217, 255);
+  fill(204, 153, 102);
   triangle (450,410,350,410,400,600);
-  ellipse(380, 300, 150, 150);
+  fill(255, 51, 133);
+  ellipse(400, 335, 150, 150);
+  fill (128, 255, 204);
+  ellipse(400, 185, 150, 150);
+  fill (230, 0, 0);
+  ellipse(400, 85, 50, 50);
+
 }
 
 function draw() {
